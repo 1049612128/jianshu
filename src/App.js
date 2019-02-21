ac9afa7a0832916b0b8660ef6f82c104c698ad34
1049612128 <1049612128@qq.com>
2019-02-21 23:59:40 +0800

    default

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import { Globalstyle } from'./style';
 class App extends Component {
   render() {
     return (
-      <div >
-        Hello world
-      </div>
+      <React.Fragment>
+      <Globalstyle />
+       <div>hello world</div>
+       </React.Fragment>
     );
   }
 }
